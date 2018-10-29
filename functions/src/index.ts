9fb387b499aa8175d650aea64a8a54377c208607
Hector Quintana <gen.cfw@gmail.com>
2018-10-28 22:42:54 -0600

    created first oncall and added functions to hosting

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,5 +4,23 @@ import * as functions from 'firebase-functions';
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+    const urlId = request.body.urlId || null;
+
+    if(!urlId){
+        response.send(`Received no url id!`);
+    } else {
+        response.send(`Received the following urlId: ${urlId}`);
+    }
+    
+});
+
+export const getUrl = functions.https.onCall((data, context) => {
+    const urlId = data.urlId || null;
+
+    if(!urlId){
+        return {message: `Received no url id!`, url: null};
+    } else {
+        return {message: `Received the following urlId: ${urlId}`, url: 'wwww.google.com'};
+    }
+    
 });
