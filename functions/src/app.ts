daddd7b33127c0f70dc0a2568eeb9eda98bd645a
Hector Quintana <gen.cfw@gmail.com>
2018-12-11 01:16:54 -0700

    added url get route

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -2,20 +2,42 @@ import * as express from "express";
 import * as cors from "cors";
 
 import { https } from './index';  
+import { db } from "./db";
 
 /* Express with CORS & automatic trailing '/' solution */
-export const apiApp = express()
-apiApp.use(cors({ origin: true }))
-apiApp.get("*", (_request, response) => {
+export const app = express()
+app.use(cors({ origin: true }))
+app.get("/", (_request, response) => {
   response.send(
     "Hello from Express on Firebase with CORS! No trailing '/' required!"
   )
-})
+});
+
+app.get("/v0/", (_request, response) => {
+  response.send(
+    "Coming soon"
+  )
+});
+
+app.get("/v0/url/:urlId/", (request, response) => {
+  const urlId = request.params.urlId;
+
+  db.child(`shortUrls/${urlId}`).once('value').then(urlSnap => {
+    if(urlSnap.val() !== null ){
+      response.send(JSON.stringify(urlSnap.val()));
+    } else {
+      response.send(JSON.stringify({error: "not found"}));
+    }
+  }).catch(err => {
+      console.log(err);
+      response.send(JSON.stringify({error: err.message}));
+  });
+});
 
 // not as clean, but a better endpoint to consume
 export const api = https.onRequest((request, response) => {
   if (!request.path) {
     request.url = `/${request.url}` // prepend '/' to keep query params if any
   }
-  return apiApp(request, response)
+  return app(request, response)
 });
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-
+// https://stackoverflow.com/questions/43486278/how-do-i-structure-cloud-functions-for-firebase-to-deploy-multiple-functions-fro
 export * from './main'; //needs to be first as it inits firebase
 export * from "./http-functions";
 export * from "./app"
